0f7d3a8692d323d23dfba5ff98c11eeae775c002
sion <sion.andreu@gmail.com>
2021-01-17 19:07:52 +0100

    Pujada inicial

---
+++ src/main/java/es/caib/notib/enviament/NotibmsEnviamentServiceApplication.java
@@ -0,0 +1,13 @@
+package es.caib.notib.enviament;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotibmsEnviamentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotibmsEnviamentServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/caib/notib/enviament/NotibmsEnviamentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package es.caib.notib.enviament;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotibmsEnviamentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
